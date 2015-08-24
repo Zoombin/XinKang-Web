abb425792b13d64f97c86df56d0c52386b6f4492
Wesley Zhang <z1000zhihe@163.com>
2015-08-24 11:59:19 +0800

    modify DB charset

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,14 @@ define('DB_NAME', 'xinkang');
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'Dsh12345');
+define('DB_PASSWORD', '');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
-define('DB_CHARSET', 'utf8mb4');
+// define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** 数据库整理类型。如不确定请勿更改 */
 define('DB_COLLATE', '');
